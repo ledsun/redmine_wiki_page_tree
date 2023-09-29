544309884c0d7f2aff996592b095d3ecd0675101
ledsun <shigeru.nakajima@gmail.com>
2023-09-30 08:58:52 +0900

    Enable to load plugin classes by zeitwerk

--- redmine_wiki_page_tree/init.rb
+++ redmine_wiki_page_tree/init.rb
@@ -1,4 +1,4 @@
-require_dependency 'redmine_wiki_page_tree_hook_listener'
+require_dependency File.expand_path('./lib/redmine_wiki_page_tree_hook_listener', __dir__)
 
 Redmine::Plugin.register :redmine_wiki_page_tree do
   name 'Redmine Wiki Page Tree Plugin'
