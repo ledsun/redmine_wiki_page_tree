52e33bbc05e9aa03f50bdb2052dec8dae82afc4f
ledsun <shigeru.nakajima@gmail.com>
2023-09-30 09:04:45 +0900

    Bump

--- redmine_wiki_page_tree/init.rb
+++ redmine_wiki_page_tree/init.rb
@@ -4,7 +4,7 @@ Redmine::Plugin.register :redmine_wiki_page_tree do
   name 'Redmine Wiki Page Tree Plugin'
   author 'Shigeru Nakajima'
   description 'Redmine plugin to show the page tree view at the sidebar.'
-  version '0.0.2'
+  version '0.0.3'
   url 'https://github.com/ledsun/redmine_wiki_page_tree'
   author_url 'https://github.com/ledsun'
 end
